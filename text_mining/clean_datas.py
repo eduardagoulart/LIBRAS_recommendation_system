cf16f419f124fdc7add46907310a34e3c1e6d602
eduardagoulart <goularteduarda.a@gmail.com>
2019-03-19 16:39:11 -0300

    retornando a query limpa

--- text_mining/clean_datas.py
+++ text_mining/clean_datas.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv('video.csv', header=None)
-print(df)
+
+def limpa_query(csv):
+    df = pd.read_csv(csv, header=None)
+    # print (type(df))
+    print (len(df))
+    for i in range(len(df)):
+        if df[9][i][0] != '#':
+            df[9][i] = None
+    df[9][0] = 'query'
+
+    return df
+
+
+limpa_query('video.csv')
