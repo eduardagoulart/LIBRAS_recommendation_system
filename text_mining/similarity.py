3dbfb12d32c6ca6a43e28c7be51f5481fd51481b
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-04 10:00:27 -0300

    similaridade entre matriz

--- text_mining/similarity.py
+++ text_mining/similarity.py
@@ -1,37 +1,72 @@
-import n_grams
+import re
+import string
 import numpy as np
-from math import sqrt
+import clean_datas
 
 
-text, _ = n_grams.description()
-
-stops = n_grams.remove_stop_words()
-vocabulary = n_grams.count_frequency(stops)
-
-
-def fit_transform(text, words):
-    return [1 if word in text.split() else 0 for word in words]
-
-
-features = []
-for i in text:
-    features.append(fit_transform(i, vocabulary))
+def clear_text(text):
+    """
+    Limpar os textos, eliminando pontuações e converter
+    todo o texto com letras minusculas.
+    """
+    pattern = "[{}]".format(string.punctuation)
+    text = [word.lower() for word in text]
+    text = [[re.sub(pattern, "", word) for word in words.split()] for words in text]
+    text = [[word for word in words if len(word) > 1] for words in text]
+    text = [' '.join(words) for words in text]
+    return np.array(text)
 
 
+new_datas = clean_datas.clean_query('video.csv')
+corpus = np.array(new_datas[6])
+corpus_clear = clear_text(corpus)
 
 
+def text_all(text):
+    """
+    Armazena em um vetor todas as palavras dos textos sem repetições.
+    """
+    text_set = set()
+    for w in [words.split() for words in text]:
+        text_set.update(w)
+    return np.array(list(text_set))
 
-def cosine_similarity(a, b):
-    if (escalar(a) != 0 and escalar(b) != 0):
-        s = float(total) / (escalar(a) * escalar(b))
+
+vocabulary = text_all(corpus_clear)
 
 
-def text_simillarities(id_text, features, tex, n_text=3):
+def fit_transform(text, words=vocabulary):
+    """
+    Converte o texto em um vetor, onde compara se cada palavra obtida no vetor de
+    todas as palavras contém ou não em cada texto.
+    Insere 1 se sim e 0 se não.
+    """
+    # return [1 if word in text.split() else 0 for word in words]
+    print([int(word in text.split()) for word in words])
+    return [int(word in text.split()) for word in words]
+
+
+features = np.array(list(map(fit_transform, corpus_clear)))
+
+print(features)
+
+
+def cosine_similarity(v, w):
+    return np.dot(v, w) / np.sqrt(np.dot(v, v) * np.dot(w, w))
+    # return np.dot(v, w)/(np.linalg.norm(v)*np.linalg.norm(w))
+
+
+def text_simillarities(id_text, features=features, text=corpus, n_text=3):
+    """
+    Dado o texto a ser analisado, a função retorna em ordem descrecente quais os demais textos são
+    similares ao analisado. A função retorna matriz de 2 por n_text, onde a primeira e a segunda coluna
+    refere-se ao texto analisado e a similaridade do texto analisado, respectivamente.
+    """
     simillarity = [[cosine_similarity(features[id_text], feature), int(i)] for i, feature in enumerate(features)]
     simillarity = np.array(sorted(simillarity, key=lambda sim: sim[0], reverse=True))
-    return [[tex[y], simillarity[x, 0]] for x, y in enumerate(np.int0(simillarity[1:1]), 1)][:n_text]
+    return [[text[y], simillarity[x, 0]] for x, y in enumerate(np.int0(simillarity[1:, 1]), 1)][:n_text]
 
 
-for te in text:
-    for t, s in text_simillarities(id_text=3, features=features, tex=te):
-        print(f'Texto: {t}, similaridade{round(s, 2)}')
+print('Texto analisado -> ', corpus[3], '\n')
+for t, s in text_simillarities(3):
+    print('Texto: {} | Similaridade: {}'.format(t, round(s, 2)))
