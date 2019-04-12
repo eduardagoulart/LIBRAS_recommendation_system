3e7968fb72075474e1680574649c0c35980a86fd
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-12 18:25:51 -0300

    idade do vídeo

--- text_mining/pre_processamento.py
+++ text_mining/pre_processamento.py
@@ -9,14 +9,12 @@ class PreProcessamentoDados:
 
     def video_age(self):
         data_time = self.file['data_de_publicação']
-        # data_time[0] = data_time[0].split(" ")
-        aux = data_time[0].split(" ")
-        # print(data_time)
+        splited_data = data_time[0].split(" ")
         abbr_to_num = {name: num for num, name in enumerate(calendar.month_abbr) if num}
-        aux[1] = aux[1].replace(",", "")
-        print(type(aux[1]))
-        age = datetime.now() - datetime(int(aux[2]), abbr_to_num[aux[0]], int(aux[1]))
-        print(age.seconds)
+        splited_data[1] = splited_data[1].replace(",", "")
+        age = datetime.now() - datetime(int(splited_data[2]), abbr_to_num[splited_data[0]], int(splited_data[1]))
+        time_in_seconds = (age.days * 86400) + age.seconds
+        return time_in_seconds
 
 
 if __name__ == '__main__':
