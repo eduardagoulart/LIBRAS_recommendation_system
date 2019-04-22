2d6a0eabbc2c9c1367cb4f1c99ca62210696ced2
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-22 07:58:30 -0300

    funcionando uma playlist sem o autor

--- text_mining/playlist.py
+++ text_mining/playlist.py
@@ -12,13 +12,14 @@ class Playlist:
 
     def playlist_generator_with_author(self):
         first = self.distances[self.first]
-        list_recomendation = [self.file["nome_do_vídeo"][self.first]]
+        list_recomendation = []
         for i in range(0, 10):
             value = max(first)
             list_recomendation.append(self.file["nome_do_vídeo"][first.index(value)])
             new = self.distances[first.index(value)]
             first[first.index(value)] = -1
             first = [first[j] + new[j] for j in range(0, len(new))]
+        print(list_recomendation)
         return list_recomendation
 
     def playlist_generator_without_author(self):
@@ -28,8 +29,9 @@ class Playlist:
             value = min(first)
             list_recomendation.append(self.file["nome_do_vídeo"][first.index(value)])
             new = self.distances[first.index(value)]
-            first[first.index(value)] = -1
+            first[first.index(value)] = 100000000000000000000
             first = [first[j] + new[j] for j in range(0, len(new))]
+        print(list_recomendation)
         return list_recomendation
 
 
@@ -40,4 +42,4 @@ if __name__ == '__main__':
         print("Argumento inválido para começar a playlist")
         exit(404)
 
-    Playlist(int(id_video)).playlist_generator_with_author()
+    Playlist(int(id_video)).playlist_generator_without_author()
