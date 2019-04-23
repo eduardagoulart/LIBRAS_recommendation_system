d5c478c26a6a008237d487687493db0b74705065
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-23 08:19:41 -0300

    adicionando a similaridade dos comentarios na playlist de autor

--- text_mining/cosine_distance.py
+++ text_mining/cosine_distance.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pre_processing import PreProcessamentoDados
+from similarity import matrix
 
 
 class VideoDistance:
@@ -16,18 +17,20 @@ class VideoDistance:
         view = self.obj.views_relative_age()
         likes = self.obj.likes_relative_views()
         deslikes = self.obj.deslikes_relative_views()
-        sum_values = [float(age[i]) - float(duration[i]) - float(view[i]) - float(likes[i]) - float(deslikes[i]) for i
-                      in range(0, len(age))]
+        comments = matrix()
+        sum_values = [[float(age[i]) - float(duration[i]) - float(view[i]) - float(likes[i]) - float(
+            deslikes[i] - float(comment[i][0])) for i in range(0, len(age))] for comment in comments]
         return sum_values
 
     def adding_authors(self):
         authors = self.obj.author()
         sum_values = self.matrix_pre_process()
-        return [[abs(float(sum_values[i]) - author[i]) for i in range(0, len(author))] for author in authors]
+        return [[abs(float(sum_values[j][i]) - authors[j][i]) for i in range(0, len(authors[j]))] for j in range(0, len(authors))]
 
     def normallize_sum_with_author(self):
         matrix = self.adding_authors()
         standard = [[value / max(internal_list) for value in internal_list] for internal_list in matrix]
+        print(standard[0])
         return standard
 
     def simillarity_matrix(self):
@@ -50,4 +53,4 @@ if "__main__" == __name__:
     # v = [2, 3, 7, 8, 9, 6]
     v = [1, 2, 3, 4, 5, 7]
     # VideoDistance().cosine_distance(u, v)
-    VideoDistance().simillarity_without_author()
+    VideoDistance().normallize_sum_with_author()
